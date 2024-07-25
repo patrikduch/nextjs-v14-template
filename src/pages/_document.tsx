b5b3dee629c4df44145a1982f0235ecd5efa4ef8
patrikduch <duchpatrik@icloud.com>
2024-07-25 17:29:18 +0200

    Material UI rendering

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,46 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+const cache = createCache({ key: 'css' });
+const { extractCritical } = createEmotionServer(cache);
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props: any) =>
+          <CacheProvider value={cache}><App {...props} /></CacheProvider>,
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style
+            data-emotion-css={styles.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: styles.css }}
+          />
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
